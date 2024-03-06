93beda0ccc58e85fe7e6c1aec15dc9cb12346d6f
ANURAG SAXENA <99179352+AnuragSaxena1999@users.noreply.github.com>
2024-03-07 00:49:08 +0530

    Update Contact.js

--- src/components/Contact1/Contact.js
+++ src/components/Contact1/Contact.js
@@ -1,65 +1,21 @@
-// // ContactMe.js
+// ContactMe.js
 
-// import React, { useState } from 'react';
-// import './ContactMe.css'; // Import your CSS file
+import React, { useState } from 'react';
+import './ContactMe.css'; // Import your CSS file
 
-// const Contact = () => {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     email: '',
-//     message: '',
-//   });
+const Contact = () => {
 
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData((prevData) => ({ ...prevData, [name]: value }));
-//   };
+  return (
+    <>
+<div className='container1'>
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     // Add your logic for handling the form submission, such as sending an email or storing the data.
-//     console.log('Form submitted:', formData);
-//     // You can add additional logic here to send the form data to a server, for example using Axios or Fetch.
-//   };
+</div>
+<div className="visme_d" data-title="Clean Contact Form" data-url="90xvevp8-clean-contact-form" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="41654" ></div>
+    
+    
+    </>
+    
+  );
+};
 
-//   return (
-//     <div className="contact-container">
-//       <h2>Contact Me</h2>
-//       <form onSubmit={handleSubmit}>
-//         <label htmlFor="name">Name:</label>
-//         <input
-//           type="text"
-//           id="name"
-//           name="name"
-//           value={formData.name}
-//           onChange={handleChange}
-//           required
-//         />
-
-//         <label htmlFor="email">Email:</label>
-//         <input
-//           type="email"
-//           id="email"
-//           name="email"
-//           value={formData.email}
-//           onChange={handleChange}
-//           required
-//         />
-
-//         <label htmlFor="message">Message:</label>
-//         <textarea
-//           id="message"
-//           name="message"
-//           value={formData.message}
-//           onChange={handleChange}
-//           rows="4"
-//           required
-//         ></textarea>
-
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Contact;
+export default Contact;
